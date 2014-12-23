917e93f600248375843b214e65020c6ea590320d
jayjay <jay@epigram.co.kr>
2014-12-23 12:09:44 +0900

    first commit

---
+++ build_android.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+NDK=$HOME/Dev/libs/android-ndk-r10d
+SYSROOT=$NDK/platforms/android-14/arch-arm/
+TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64
+function build_one
+{
+	./configure \
+		--prefix=$PREFIX \
+		--disable-shared \
+		--enable-static \
+		--disable-doc \
+		--disable-ffmpeg \
+		--disable-ffplay \
+		--disable-ffprobe \
+		--disable-ffserver \
+		--disable-avdevice \
+		--disable-symver \
+		--cross-prefix=$TOOLCHAIN/bin/arm-linux-androideabi- \
+		--target-os=linux \
+		--arch=arm7-a \
+		--enable-cross-compile \
+		--sysroot=$SYSROOT \
+		--extra-cflags="-Os -fpic $ADDI_CFLAGS" \
+		--extra-ldflags="$ADDI_LDFLAGS" \
+		$ADDITIONAL_CONFIGURE_FLAG
+	make clean
+	make
+	make install
+}
+CPU=arm
+PREFIX=$(pwd)/android/$CPU
+ADDI_CFLAGS="-marm"
+build_one
